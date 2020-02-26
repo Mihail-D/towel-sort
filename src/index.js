3e4bf28a83a9332bc1eaae6118dc60935f6f6a27
Mihail-D <nuby.4register@gmail.com>
2020-02-26 17:02:35 +0300

    feat: complete 'Towel Sort' task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-module.exports = function towelSort (matrix) {
+module.exports = function towelSort(matrix) {
+    let newArr = [];
 
+    if (matrix === undefined) {
+        return [];
+    }
 
-}
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) {
+            newArr.push(matrix[i]);
+        } else {
+            matrix[i].reverse();
+            newArr.push(matrix[i]);
+        }
+    }
+
+    return newArr.flat();
+};
